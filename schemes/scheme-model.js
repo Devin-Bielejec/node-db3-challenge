1d1920b84bc7bf8e145d6437bad871df38c2ffa6
devin-bielejec <devin.bieleje@gmail.com>
2019-09-20 09:35:02 -0400

    findById and findSteps

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -13,4 +13,15 @@ const find = () => {
     db('schemes');
 }
 
-const 
+const findById = id => {
+    db("schemes")
+    .where({ id })
+    .first();
+}
+
+const findSteps = scheme_id => {
+    db("schemes as s")
+    .join("steps as st", "s.id", "st.scheme_id")
+    .select("s.id, s.scheme_name, st.step_number, st.instructions")
+    .where({ id })
+}
